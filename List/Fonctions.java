326e5d7c791fa364f1b878b3d66a47b99d3b4ed4
David Dehoust <dehoustdavid@gmail.com>
2023-01-31 15:23:25 +0100

    update

--- List/Fonctions.java
+++ List/Fonctions.java
@@ -1,13 +1,15 @@
 import java.util.List;
 
 /**
- * Cette fonction sert à afficher le contenu d'une liste que l'on renseigne en paramètre.
- * Le 'T' entre chevrons permet d'accepter plusieurs type de List,
- * aussi bien des 'int' que des 'String'.
+ * Cours sur les listes - les fonctions utiles.
  */
-
 public class Fonctions {
 
+    /**
+     * Cette fonction sert à afficher le contenu d'une liste que l'on renseigne en paramètre.
+     * Le 'T' entre chevrons permet d'accepter plusieurs type de List,
+     * aussi bien des 'int' que des 'String'.
+     */
     public static <T> void PrintList(List<T> _listToPrint) {
 
         for (int i = 0; i < _listToPrint.size(); i++) {
